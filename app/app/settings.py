8a9899fa398fbd2dcc6253f2a611f52f355ffc37
Marvin Palmer <mpalmer@glasfunds.com>
2024-09-10 17:10:29 -0500

    fixing spelling spectacular

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
+    'django.contrib.staticfiles',    
     'rest_framework',
     'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectactular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
